75cfbe00af7130bcb320829bfca1425dee348479
D3ad5h0t <alexgl007@mail.ru>
2020-04-11 13:09:50 +1000

    Master

--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import {createStore, compose, applyMiddleware} from "redux";
+import {Provider} from 'react-redux'
+import customRootReducer from './store/reducers/customRootReducer'
+import  thunk from 'redux-thunk'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(
+    customRootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root')
+);
+
 serviceWorker.unregister();
